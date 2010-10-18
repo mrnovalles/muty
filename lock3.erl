66c7493d664e4fdd7e218fca7d213714098ead4e
Mariano <zucaritas@blackeee.(none)>
2010-10-18 22:17:35 +0200

    Revert "Revert "Not working. problems with ok(Waiting) and more""
    This reverts commit 545a79021defcdbc17bc7bdbb7c4e2d4c306a148.

--- lock3.erl
+++ lock3.erl
@@ -24,9 +24,10 @@ open(Nodes, MyId, MyClock) ->
 requests(Nodes, MyId, MyClock) ->	%send a request message to all locks
 	lists:map(fun(P) -> R = make_ref(), P ! {request, self(), R, MyId, MyClock}, R end, Nodes).	%I send my ID together with the request message.
 
-wait(Nodes, Master, [], Waiting, MyId,MyClock, MyReqClock) ->	%waiting state with empty list of locks -- all of the locks sent me an ok message!! -> I can take the lock!
+wait(Nodes, Master, [], Waiting, MyId, MyClock, MyReqClock) ->	%waiting state with empty list of locks -- all of the locks sent me an ok message!! -> I can take the lock!
 	Master ! taken,		%the lock is taken
 	held(Nodes, Waiting, MyId, MyClock);	%enter the held state
+
 
 wait(Nodes, Master, Refs, Waiting, MyId, MyClock, MyReqClock) ->	%waiting for ok messages
 	receive
@@ -35,12 +36,10 @@ wait(Nodes, Master, Refs, Waiting, MyId, MyClock, MyReqClock) ->	%waiting for ok
 				NewClock = lists:max([MyClock,Clock]) + 1, %%
 			if 
 				MyReqClock > Req_Clock ->
-					From ! {ok, Ref, NewClock} %we send Ok and also the NewClock value to the other process to update its clock
+					From ! {ok, Ref, NewClock}, %we send Ok and also the NewClock value to the other process to update its clock
 					Ref4 = requests([From], MyId, MyReqClock), %% MyReqClock or NewClock ? WHICH ONE
 					wait(Nodes,Master, Ref4, Waiting, MyId, NewClock, MyReqClock);
-				MyReqClock < Req_Clock -> 
-					wait(Nodes,Master, Ref4, Waiting, MyId, NewClock, MyReqClock);
-				true ->
+				MyReqClock == Req_Clock -> 
 					if
 					MyId > Req_Id ->	%The requesting lock has higher priority
 						From ! {ok, Ref, NewClock},%send an ok message to it!	
@@ -51,26 +50,31 @@ wait(Nodes, Master, Refs, Waiting, MyId, MyClock, MyReqClock) ->	%waiting for ok
 						% NOT back to open state -> back to WAIT state!!!
 		
 					true ->	%I have higher priority: I keep the request and I go on :p
-						wait(Nodes, Master, Refs, [{From, Ref}|Waiting], MyId,NewClock, MyReqClock)
+						wait(Nodes, Master, Refs, [{From, Ref, Req_Clock}|Waiting], MyId,NewClock, MyReqClock);
 					end;
+					
+				true -> %% the req is 
+					wait(Nodes,Master, Ref, Waiting, MyId, NewClock, MyReqClock);
 			end;	
+		
 		{ok, Ref, Clock} ->	%i received an ok message from a lock
 			NewClock2 = lists:max([MyClock,Clock]) + 1,
 			Refs2 = lists:delete(Ref, Refs),	%and I delete it from my list
 			wait(Nodes, Master, Refs2, Waiting, MyId, NewClock2, MyReqClock);	%waiting for the rest of the ok messages
+		
 		release ->	%I have to release the lock
 			ok(Waiting),	%I sent ok messages to the locks that requested me while I was waiting
-			open(Nodes, MyId, NewClock)	%back to open state
+			open(Nodes, MyId, NewClock),	%back to open state
 	end.
 
 ok(Waiting) ->	%send ok message
-	lists:map(fun({F,R}) -> F ! {ok, R} end, Waiting).
+	lists:map(fun({F,R,RClock}) -> F ! {ok, R, RClock} end, Waiting).
 
 held(Nodes, Waiting, MyId, MyClock) ->	%The lock is mine!!!
 	receive
-		{request, From, Ref,_,ReqClock} ->	%I keep on accepting requests from other locks to send them ok afterwards
-			held(Nodes, [{From, Ref}|Waiting], MyId, MyClock  );
+		{request, From, Ref,_ ,ReqClock} ->	%I keep on accepting requests from other locks to send them ok afterwards
+			held(Nodes, [{From, Ref, ReqClock}|Waiting], MyId, MyClock  );
 		release ->	%release message from the worker -> I have to release the lock
-			ok(Waiting),	%I inform the waiting locks!
+			ok(Waiting),			%I inform the waiting locks!
 			open(Nodes, MyId, MyClock)	%back to open state!!
 	end.
