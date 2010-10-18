dca18ddf5e5fa924a4c8aedc24966dd75de92e08
Mariano <zucaritas@blackeee.(none)>
2010-10-18 21:38:20 +0200

    lock3 might be ready

--- lock3.erl
+++ lock3.erl
@@ -30,44 +30,47 @@ wait(Nodes, Master, [], Waiting, MyId,MyClock, MyReqClock) ->	%waiting state wit
 
 wait(Nodes, Master, Refs, Waiting, MyId, MyClock, MyReqClock) ->	%waiting for ok messages
 	receive
-	%Req msg now haas MyId and also MyClock to compare to it.
+	%Req msg now has MyId and also MyClock to compare to it.
 		{request, From, Ref, Req_Id, Req_Clock} ->
-			NewClock = lists:max([MyClock,Clock]) + 1, %%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+				NewClock = lists:max([MyClock,Clock]) + 1, %%
 			if 
 				MyReqClock > Req_Clock ->
-				From ! {ok, Ref, NewClock} %we send the New Clock Value value to the other process to update its clock			
-				
-			if
-				MyId > Req_Id ->	%The requesting lock has higher priority
-				From ! {ok, Ref},	%send an ok message to it!	
-				%send another request to the lock with higher priority
-				%to ensure the "happened before" order
-				Ref2 = requests([From], MyId),
-				wait(Nodes, Master, Ref2, Waiting, MyId);	%and enter waiting state
-				% NOT back to open state -> back to WAIT state!!!
-			
-				true ->	%I have higher priority: I keep the request and I go on :p
-				wait(Nodes, Master, Refs, [{From, Ref}|Waiting], MyId)
-			end;
-		{ok, Ref} ->	%i received an ok message from a lock
+					From ! {ok, Ref, NewClock} %we send Ok and also the NewClock value to the other process to update its clock
+					Ref4 = requests([From], MyId, MyReqClock), %% MyReqClock or NewClock ? WHICH ONE
+					wait(Nodes,Master, Ref4, Waiting, MyId, NewClock, MyReqClock);
+				MyReqClock < Req_Clock -> 
+					wait(Nodes,Master, Ref4, Waiting, MyId, NewClock, MyReqClock);
+				true ->
+					if
+					MyId > Req_Id ->	%The requesting lock has higher priority
+						From ! {ok, Ref, NewClock},%send an ok message to it!	
+									%send another request to the lock with higher priority
+									%to ensure the "happened before" order
+						Ref2 = requests([From], MyId, MyReqClock),
+						wait(Nodes, Master, Ref2, Waiting, MyId, NewClock, MyReqClock);	%and enter waiting state
+						% NOT back to open state -> back to WAIT state!!!
+		
+					true ->	%I have higher priority: I keep the request and I go on :p
+						wait(Nodes, Master, Refs, [{From, Ref}|Waiting], MyId,NewClock, MyReqClock)
+					end;
+			end;	
+		{ok, Ref, Clock} ->	%i received an ok message from a lock
+			NewClock2 = lists:max([MyClock,Clock]) + 1,
 			Refs2 = lists:delete(Ref, Refs),	%and I delete it from my list
-			wait(Nodes, Master, Refs2, Waiting, MyId);	%waiting for the rest of the ok messages
+			wait(Nodes, Master, Refs2, Waiting, MyId, NewClock2, MyReqClock);	%waiting for the rest of the ok messages
 		release ->	%I have to release the lock
 			ok(Waiting),	%I sent ok messages to the locks that requested me while I was waiting
-			open(Nodes, MyId)	%back to open state
+			open(Nodes, MyId, NewClock)	%back to open state
 	end.
 
 ok(Waiting) ->	%send ok message
 	lists:map(fun({F,R}) -> F ! {ok, R} end, Waiting).
 
-held(Nodes, Waiting, MyId) ->	%The lock is mine!!!
+held(Nodes, Waiting, MyId, MyClock) ->	%The lock is mine!!!
 	receive
-		{request, From, Ref, _} ->	%I keep on accepting requests from other locks to send them ok afterwards
-			held(Nodes, [{From, Ref}|Waiting], MyId);
+		{request, From, Ref,_,ReqClock} ->	%I keep on accepting requests from other locks to send them ok afterwards
+			held(Nodes, [{From, Ref}|Waiting], MyId, MyClock  );
 		release ->	%release message from the worker -> I have to release the lock
 			ok(Waiting),	%I inform the waiting locks!
-			open(Nodes, MyId)	%back to open state!!
+			open(Nodes, MyId, MyClock)	%back to open state!!
 	end.
